c3a3b46c8f55c5e38708bf4cf647abeebce27ca1
MarquesThomasRestart <127489904+MarquesThomasRestart@users.noreply.github.com>
2023-05-21 15:32:19 +0200

    Delete templates directory

--- templates/style.php
+++
@@ -1,4 +0,0 @@
-<link href="https://fonts.googleapis.com/css?family=Farro&display=swap" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.2/css/all.css">
-<link rel="stylesheet" href="css/style-retwitt.css">
